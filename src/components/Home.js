00fde8eb1fbabcf3b3bc342e7400b3d82e7d41f3
Stubblefish <george.l.wise@gmail.com>
2021-08-30 18:06:37 -0700

    Finished project page, schmea, post page and post schema

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
+import image from "../firewatch.jpg";
 
 export default function Home() {
   return (
     <main>
-      <img />
-      <section>
-        <h1>Hi! I'm George</h1>
+      <img
+        src={image}
+        alt="Overlooking the forest"
+        className="absolute object-cover w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hi! I'm George
+        </h1>
       </section>
     </main>
   );
