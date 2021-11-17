033901c81c551b55a72c6237f04fcdfbca854607
Stubblefish <george.l.wise@gmail.com>
2021-11-17 15:30:02 -0800

    Added font styling to make home page more interesting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import image from "../firewatch.jpg";
+import "../HoverEffect.css";
 
 export default function Home() {
   return (
@@ -10,9 +11,52 @@ export default function Home() {
         className="absolute object-cover w-full h-full"
       />
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
-          Hi! I'm George
-        </h1>
+        <div class="foo">
+          <div>
+            <span class="letter" data-letter="H">
+              H
+            </span>
+            <span class="letter" data-letter="I">
+              I
+            </span>
+
+            <span className="mr-16" data-letter=""></span>
+            <span class="letter" data-letter="I">
+              I
+            </span>
+            <span class="letter" data-letter="'">
+              '
+            </span>
+            <span class="letter" data-letter="M">
+              M
+            </span>
+            <span className="mr-16" data-letter=""></span>
+            <span class="letter" data-letter="G">
+              G
+            </span>
+            <span class="letter" data-letter="E">
+              E
+            </span>
+            <span class="letter" data-letter="O">
+              O
+            </span>
+            <span class="letter" data-letter="R">
+              R
+            </span>
+            <span class="letter" data-letter="G">
+              G
+            </span>
+            <span class="letter" data-letter="E">
+              E
+            </span>
+            <span class="letter" data-letter="!">
+              !
+            </span>
+          </div>
+          <div className="text-green-100 bg-green-700 py-2 text-2xl">
+            And welcome to my portfolio!
+          </div>
+        </div>
       </section>
     </main>
   );
